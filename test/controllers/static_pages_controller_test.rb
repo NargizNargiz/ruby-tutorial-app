ec35b9b8a3928021559988064927d345df3f87bb
Nargiz <nargiz1397@mail.ru>
2019-12-29 16:55:07 +0300

    add tests for controller static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  # гет означает что тесты ожидают от страниц хом о хелп поведеление обычных веб-страниц и ответ запроса должен быть саксесс
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+  
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+
+  end
+  test "should get about" do 
+  	get static_pages_about_url
+  	assert_response :success
+    # check titlres from views
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+
   end
 
 end
