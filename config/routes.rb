c28e45bdaecd2a23ce9f2e71f9a0ce69596141b8
Nargiz <nargiz1397@mail.ru>
2020-01-17 14:05:57 +0300

    Finish layot and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
 	# метод рут направляет рут-путь к контроллеру и дейтсвию по нашему выбора 
 	# в даннном случае к хоме	
-  root 'static_pages#home'
+  root             'static_pages#home'
   get 'help' => 'static_pages#help'
-  # штука сверху создаст два именнованых маршрута - help_path and help_url
-  # 
-  # 
+  # штука сверху создаст два именнованых маршрута - help_path and help_url 
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  get 'signup' => 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,26 +4,26 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   # гет означает что тесты ожидают от страниц хом о хелп поведеление обычных веб-страниц и ответ запроса должен быть саксесс
 
   test "should get home" do
-    get static_pages_home_url
+    get root_url
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial Sample App"
   
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_url
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
 
   end
   test "should get about" do 
-  	get static_pages_about_url
+  	get about_url
   	assert_response :success
     # check titlres from views
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_url
     assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
